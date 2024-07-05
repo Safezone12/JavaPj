9dab8eec0deacacd39f1f70fb5d7d43d3d50ac85
Safezone12 <jkplol12@gmail.com>
2024-07-05 23:36:52 +0700

    add lastname.java

--- jak.java
+++ jak.java
@@ -1,5 +1,5 @@
 public class jak {
      public static void main(String[] args) {
-        System.out.println("My name is Jakkapat");
+        System.out.println("My name is Jakkapat Charoentri");
      }   
 }
